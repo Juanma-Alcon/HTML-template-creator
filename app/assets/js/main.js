372439d6b79357d479823d95fd2018a866dabdf4
Juanma Alcon <juanmanuel.alcon@rastreator.com>
2019-04-25 13:12:05 +0200

    fixes

---
+++ app/assets/js/main.js
@@ -0,0 +1,19 @@
+const $ = require('jquery')
+
+window.$ = $
+window.jQuery = $
+
+$(document).ready(() => {
+  // const views = [
+  //   {
+  //     sect: '.cj-full-searcher',
+  //     module: require('./components/cj/input/searcher/script')
+  //   }
+  // ]
+
+  _.each(views, (section) => {
+    const view = $(section.sect)
+    view.length && section.module.init(view)
+  })
+
+})
