4190a8736bcb186b47e63b9a01795aaa8e16d459
Juanma Alcon <juanmanuel.alcon@rastreator.com>
2019-04-25 13:19:41 +0200

    Fix

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,15 +1,16 @@
 const $ = require('jquery')
+const _ = require('underscore')
 
 window.$ = $
 window.jQuery = $
 
 $(document).ready(() => {
-  // const views = [
+  const views = [
   //   {
   //     sect: '.cj-full-searcher',
   //     module: require('./components/cj/input/searcher/script')
   //   }
-  // ]
+  ]
 
   _.each(views, (section) => {
     const view = $(section.sect)
